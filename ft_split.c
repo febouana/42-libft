c8d918692f62d53166c0c48daa7467edab7418d5
justaPoet <fehim.bouanani@gmail.com>
2024-08-27 20:07:57 +0200

    new-split

--- ft_split.c
+++ ft_split.c
@@ -3,100 +3,99 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: febouana <febouana@student.42.fr>          +#+  +:+       +#+        */
+/*   By: apoet <apoet@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 18:20:29 by febouana          #+#    #+#             */
-/*   Updated: 2023/11/24 17:05:41 by febouana         ###   ########.fr       */
+/*   Updated: 2024/08/27 20:06:38 by apoet            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_var(size_t *i, int *j, int *s_word)
+static void ft_init_vars(size_t *i, size_t *j, int *start)
 {
-	*i = 0;
-	*j = 0;
-	*s_word = -1;
+    *i = 0;
+    *j = 0;
+    *start = 0;
 }
 
-static void	*ft_free(char **res, int count)
+static void *ft_free_tabtab(char **result, int count) //*OKOK
 {
-	int	i;
+    int i;
 
-	i = 0;
-	while (i < count)
-	{
-		free(res[i]);
-		i++;
-	}
-	free(res);
-	return (NULL);
+    i = 0;
+    while (i < count)
+    {
+        free(result[i]);
+        i++;
+    }
+    free(result);
+    return (NULL);
 }
 
-static char	*ft_new_word(const char *str, int start, int end)
+static size_t count_words(char *str, char separator) //* OKOK
 {
-	char	*word;
-	int		i;
+    size_t i;
+    size_t counter;
 
-	i = 0;
-	word = malloc((end - start + 1) * sizeof(char));
-	if (!word)
-		return (NULL);
-	while (start < end)
-	{
-		word[i] = str[start];
-		i++;
-		start++;
-	}
-	word[i] = '\0';
-	return (word);
+    i = 0;
+    counter = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] != separator)
+        {
+            counter++;
+            while (str[i] != separator && str[i] != '\0')
+                i++;
+        }
+        else 
+            while (str[i] == separator && str[i] != '\0')
+                i++;
+    }            
+    return (counter);
 }
 
-static int	ft_counter(const char *s, char c)
+static char *set_word(char *str, int start, int end) //* OKOK
 {
-	int	count;
-	int	x;
+    char *word;
+    int i;
 
-	count = 0;
-	x = 0;
-	while (*s)
-	{
-		if (*s != c && x == 0)
-		{
-			x = 1;
-			count++;
-		}
-		else if (*s == c)
-			x = 0;
-		s++;
-	}
-	return (count);
+    i = 0;
+    word = malloc((end - start + 1) * sizeof(char));
+    while (start < end)
+    {
+        word[i] = str[start];
+        start++;
+        i++;
+    }
+    word[i] = '\0';
+    return (word);
 }
 
-char	**ft_split(const char *s, char c)
+static char **ft_split(char *str, char audd)
 {
-	char	**res;
-	size_t	i;
-	int		j;
-	int		s_word;
-
-	ft_var(&i, &j, &s_word);
-	res = ft_calloc((ft_counter(s, c) + 1), sizeof(char *));
-	if (!res)
-		return (NULL);
-	while (i <= ft_strlen(s))
-	{
-		if (s[i] != c && s_word < 0)
-			s_word = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && s_word >= 0)
-		{
-			res[j] = ft_new_word(s, s_word, i);
-			if (!(res[j]))
-				return (ft_free(res, j));
-			s_word = -1;
-			j++;
-		}
-		i++;
-	}
-	return (res);
+    size_t i;
+    size_t j;
+    int start;
+    char **result; 
+    
+    ft_init_vars(&i, &j, &start);
+    result = ft_calloc(count_words(str, audd) + 1, sizeof(char *));
+    if (!result)
+        return (NULL);
+    while (str[i] != '\0')
+    {
+        if (str[i] == audd)
+            i++;
+        else
+        {   
+            start = i;
+            while (str[i] != audd && str[i] != '\0')
+                i++;
+            result[j++] = set_word(str, start, i);
+            if (!result[j - 1])
+                return (ft_free_tabtab(result, j));
+        }
+    }
+    return (result);
 }
